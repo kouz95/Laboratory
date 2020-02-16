07f5328e5ff7b54b84d43b5fdc02fea3e9747dd1
kouz <lastpuzly@gmail.com>
2020-02-16 15:47:27 +0900

    feat: use Comparator

---
+++ src/main/java/modern/lambda/Apple.java
@@ -0,0 +1,13 @@
+package modern.lambda;
+
+public class Apple {
+	private final Integer weight;
+
+	public Apple(Integer weight) {
+		this.weight = weight;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+}
---
+++ src/main/java/modern/lambda/UsingComparator.java
@@ -0,0 +1,31 @@
+package modern.lambda;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class UsingComparator {
+	public static void main(String[] args) {
+		Comparator<Apple> byWeight =
+			Comparator.comparing(Apple::getWeight);
+
+		List<Apple> apples = new ArrayList<>();
+		Apple a1 = new Apple(150);
+		Apple a2 = new Apple(100);
+		Apple a3 = new Apple(200);
+		apples.add(a1);
+		apples.add(a2);
+		apples.add(a3);
+
+		apples.stream()
+			.mapToInt(Apple::getWeight)
+			.forEach(System.out::println);
+
+		apples.sort(byWeight);
+
+		apples.stream()
+			.mapToInt(Apple::getWeight)
+			.forEach(System.out::println);
+	}
+}
+
