3875674aabb479727d8ffc018f82e7ec64eace6e
kouz <lastpuzly@gmail.com>
2020-03-05 15:59:48 +0900

    refactor: not Digit include negative numbers

--- src/main/java/chicken/domain/Price.java
+++ src/main/java/chicken/domain/Price.java
@@ -1,27 +1,16 @@
 package chicken.domain;
 
+import chicken.util.StringUtils;
+
 public class Price {
-	public static final String MIN_AMOUNT = "가격은 0 보다 작 수 없습니다.";
 	public static final String NOT_NUMBER = "가격은 숫자이어야 합니다.";
 
 	private final int price;
 
-	public Price(String price) {
-		checkValidityOf(price);
-		this.price = Integer.parseInt(price);
-	}
-
-	private void checkValidityOf(String price) {
-		if (isNotDigit(price)) {
+	public Price(String line) {
+		if (StringUtils.isNotDigit(line)) {
 			throw new NumberFormatException(NOT_NUMBER);
 		}
-		if (Integer.parseInt(price) < 0) {
-			throw new IllegalArgumentException(MIN_AMOUNT);
-		}
-	}
-
-	private boolean isNotDigit(String price) {
-		return price.chars()
-			.anyMatch(ch -> !Character.isDigit(ch));
+		this.price = Integer.parseInt(line);
 	}
 }
--- src/test/java/chicken/domain/PriceTest.java
+++ src/test/java/chicken/domain/PriceTest.java
@@ -7,15 +7,12 @@ import org.junit.jupiter.api.Test;
 
 class PriceTest {
 	@Test
-	void throwExceptionWhenUnderZero() {
-		assertThatIllegalArgumentException().isThrownBy(() ->
-			new Price("-1")
-		).withMessage(MIN_AMOUNT);
-	}
-
-	@Test
 	void throwExceptionWhenNotNumber() {
 		assertThatThrownBy(() -> new Price("a"))
+			.isInstanceOf(NumberFormatException.class)
+			.hasMessage(NOT_NUMBER);
+
+		assertThatThrownBy(() -> new Price("-1"))
 			.isInstanceOf(NumberFormatException.class)
 			.hasMessage(NOT_NUMBER);
 	}
