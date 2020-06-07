950d3b06ac6e985d8b3fe9c63a082a8b77d2be6f
kouz <lastpuzly@gmail.com>
2020-06-08 00:29:42 +0900

    feat: spring data jdbc, delete RefAggregate example

--- src/main/java/spring/data/jdbc/my/infra/MyAggregateBeforeDeleteCallback.java
+++ src/main/java/spring/data/jdbc/my/infra/MyAggregateBeforeDeleteCallback.java
@@ -1,22 +1,22 @@
 package spring.data.jdbc.my.infra;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.relational.core.conversion.AggregateChange;
-import org.springframework.data.relational.core.conversion.DbAction;
 import org.springframework.data.relational.core.mapping.event.BeforeDeleteCallback;
 import org.springframework.stereotype.Component;
 
 import spring.data.jdbc.my.domain.MyAggregate;
-import spring.data.jdbc.ref.domain.RefAggregate;
 import spring.data.jdbc.ref.domain.RefAggregateRepository;
 
+@Component
 public class MyAggregateBeforeDeleteCallback implements BeforeDeleteCallback<MyAggregate> {
+    @Autowired
+    private RefAggregateRepository refAggregateRepository;
 
     @Override
-    public MyAggregate onBeforeDelete(MyAggregate aggregate, AggregateChange<MyAggregate> aggregateChange) {
+    public MyAggregate onBeforeDelete(MyAggregate aggregate,
+        AggregateChange<MyAggregate> aggregateChange) {
+        refAggregateRepository.deleteByMyAggregateId(aggregate.getId());
         return aggregate;
     }
 }
--- src/test/java/spring/data/jdbc/my/application/MyAggregateServiceTest.java
+++ src/test/java/spring/data/jdbc/my/application/MyAggregateServiceTest.java
@@ -1,11 +1,9 @@
 package spring.data.jdbc.my.application;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import spring.data.jdbc.ref.application.RefAggregateService;
@@ -20,13 +18,14 @@ class MyAggregateServiceTest {
     private RefAggregateService refAggregateService;
 
     @Test
-    void name() {
+    void test() {
         Long myAggregateId = myAggregateService.create("test");
         Long refAggregateId = refAggregateService.create(myAggregateId);
         RefAggregate refAggregate = refAggregateService.findById(refAggregateId);
         assertThat(refAggregate.getId()).isNotNull();
 
         myAggregateService.delete(myAggregateId);
+
         assertThatThrownBy(() -> refAggregateService.findById(refAggregateId))
         .isInstanceOf(RuntimeException.class)
         .hasMessage("not exist");
